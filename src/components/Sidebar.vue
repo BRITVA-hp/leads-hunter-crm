dffd7d5dbde2b74d184d7c27004dd3fcf23f222e
BRITVA-hp <borbritvahp@gmail.com>
2023-02-17 19:15:33 +0400

    sidebar settings

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    :class="{ 'sidebar--active' : sidebarMini}"
+    :class="{ 'sidebar--active' : sidebarActive}"
     class="sidebar bg-gradient-to-b from-zinc-700 to-zinc-900 text-white">
 
     <div class="sidebar--active__item flex justify-between items-center p-2.5 border-b border-gray-500">
       <router-link class="sidebar--active__hide" to="/"><img class="w-20" src="../assets/img/logo.svg" alt="leads hunter"></router-link>
       <div
-        @click="sidebarMini = !sidebarMini"
+        @click.prevent="sidebarActive = !sidebarActive"
         class="sidebar__burger">
         <div class="sidebar__burger__elem sidebar__burger__elem--1"></div>
         <div class="sidebar__burger__elem sidebar__burger__elem--2"></div>
@@ -14,18 +14,33 @@
       </div>
     </div>
 
-    <div class="sidebar--active__item accordion" id="sidenavUser">
+    <div
+      :class="{'hover:bg-zinc-600 duration-300' : sidebarActive}"
+      class="sidebar--active__item accordion" id="sidenavUser">
 
-      <router-link to="/" class="sidebar--active__show w-10 h-10 overflow-hidden flex justify-center items-center">
+      <router-link to="/" class="sidebar--active__show w-10 h-10 overflow-hidden flex justify-center items-center hover:bg-zinc-600 duration-300">
         <img src="../assets/img/avatar-default.png" alt="avatar">
       </router-link>
 
-      <button class="accordion-button collapsed sidebar__user__button sidebar--active__hide w-full flex justify-between items-center gap-x-2.5 p-2.5 border-b border-gray-500"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#sidenavUserMenu"
-              aria-expanded="false"
-              aria-controls="sidenavUserMenu">
+      <button class="
+        accordion-button
+        collapsed
+        sidebar__user__button
+        sidebar--active__hide
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        p-2.5
+        border-b
+        border-gray-500
+        hover:bg-zinc-600
+        duration-300"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#sidenavUserMenu"
+        aria-expanded="false"
+        aria-controls="sidenavUserMenu">
 
         <div class="w-10 h-10 overflow-hidden flex justify-center items-center">
           <img src="../assets/img/avatar-default.png" alt="avatar">
@@ -33,44 +48,305 @@
 
         <p class="text-ellipsis overflow-hidden whitespace-nowrap">Тестовый пользователь</p>
       </button>
-      <div id="sidenavUserMenu" class="sidebar--active__hide accordion-collapse collapse max-h-80 overflow-auto"
+      <div id="sidenavUserMenu" class="sidebar--active__hide accordion-collapse collapse max-h-80 overflow-auto border-b border-gray-500"
            data-bs-parent="#sidenavUser">
-        <strong>This is the first item's accordion body.</strong> It is shown by default,
-        until the collapse plugin adds the appropriate classes that we use to style each
-        element. These classes control the overall appearance, as well as the showing and
-        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-        our default variables. It's also worth noting that just about any HTML can go within
-        the <code>.accordion-body</code>, though the transition does limit overflow.
+        <ul class="p-2.5 text-gray-100">
+          <li>
+              <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              <span class="material-icons-outlined">
+              account_circle
+              </span>
+              <span>Профиль</span>
+            </router-link>
+          </li>
+          <li>
+            <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              <span class="material-icons-outlined">
+              manage_accounts
+              </span>
+              <span>Настройки аккаунта</span>
+            </router-link>
+          </li>
+          <li>
+            <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              <span class="material-icons-outlined">
+              logout
+              </span>
+              <span>Выйти</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
     </div>
 
-<!--    <div class="flex justify-between items-center gap-x-2.5 p-2.5 border-b border-gray-500">-->
-<!--      <div class="w-10 h-10 overflow-hidden flex justify-center items-center">-->
-<!--        <img src="../assets/img/avatar-default.png" alt="avatar">-->
-<!--      </div>-->
+    <div
+      :class="{ 'hover:bg-zinc-600 duration-300' : sidebarActive }"
+      class="sidebar--active__item dropdown relative">
 
-<!--      <div class="accordion" id="accordionExample">-->
-<!--        <div class="accordion-item">-->
-<!--          <div class="accordion-header" id="headingOne">-->
-<!--            <p class="text-ellipsis overflow-hidden whitespace-nowrap accordion-button " data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"-->
-<!--               aria-controls="collapseOne">Тестовый пользователь sdfg sdfsd sdfsdf sdfsdf dfsfd sdfsdf</p>-->
-<!--          </div>-->
+      <button
+        class="
+        sidebar--active__show
+        dropdown-toggle
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        border-gray-500 hover:bg-zinc-600
+        duration-300
+        justify-center"
+        type="button"
+        id="sidenavProjectsButton"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">
 
-<!--          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"-->
-<!--               data-bs-parent="#accordionExample">-->
-<!--            <div class="accordion-body py-4 px-5">-->
-<!--              <strong>This is the first item's accordion body.</strong> It is shown by default,-->
-<!--              until the collapse plugin adds the appropriate classes that we use to style each-->
-<!--              element. These classes control the overall appearance, as well as the showing and-->
-<!--              hiding via CSS transitions. You can modify any of this with custom CSS or overriding-->
-<!--              our default variables. It's also worth noting that just about any HTML can go within-->
-<!--              the <code>.accordion-body</code>, though the transition does limit overflow.-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
+        <span class="material-icons-outlined">
+        format_list_bulleted
+        </span>
+      </button>
 
-<!--    </div>-->
+      <button
+        class="
+        dropdown-toggle
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        border-gray-500 hover:bg-zinc-600
+        duration-300
+        p-2.5
+        pr-8
+        border-b
+        sidebar--active__hide"
+        type="button"
+        id="sidenavProjectsButton"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">
+
+        <span class="material-icons-outlined">
+        format_list_bulleted
+        </span>
+
+        <p class="text-ellipsis overflow-hidden whitespace-nowrap sidebar--active__hide">Тестовый проект dsfg dfs</p>
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="caret-down"
+          class="w-2 ml-2 absolute right-4 bottom-1/2 translate-y-2/4 sidebar--active__hide"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+        >
+          <path
+            fill="currentColor"
+            d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+          ></path>
+        </svg>
+      </button>
+      <ul id="sidenavProjectsList"
+          class="
+          dropdown-menu
+          bg-zinc-500
+          shadow-xl
+          max-w-xs
+          absolute
+          hidden
+          max-h-80
+          overflow-auto
+          border
+          border-gray-500
+          p-2
+          text-gray-100"
+           aria-labelledby="sidenavProjectsButton">
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            1. Ещё проект
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            2. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            3. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            4. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            5. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            6. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            7. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            8. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            9. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            10. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            11. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            12. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            13. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            14. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+        <li class="dropdown-item">
+          <router-link class="block w-full text-sm leading-none hover:bg-zinc-700 p-1.5 duration-300 rounded text-ellipsis overflow-hidden whitespace-nowrap" to="/">
+            15. И ещё проект вапрапр вапвап вапва вапвап вапеукеукеуке упвап
+          </router-link>
+        </li>
+      </ul>
+    </div>
+
+    <div class="hover:bg-zinc-600 duration-300">
+      <router-link to="/"
+        class="
+        sidebar--active__hide
+        p-2.5
+        border-b
+        border-gray-500
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        border-gray-500">
+        <span class="material-icons-outlined">list_alt</span>
+        <span>ЕЖЛ</span>
+      </router-link>
+      <router-link to="/"
+        class="
+        sidebar--active__show
+        p-2.5
+        border-gray-500
+        w-full
+        flex
+        items-center
+        justify-center">
+        <span class="material-icons-outlined">list_alt</span>
+      </router-link>
+    </div>
+
+    <div class="hover:bg-zinc-600 duration-300">
+      <router-link to="/"
+                   class="
+        sidebar--active__hide
+        p-2.5
+        border-b
+        border-gray-500
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        border-gray-500">
+        <span class="material-icons-outlined">sort</span>
+        <span>Лог</span>
+      </router-link>
+      <router-link to="/"
+                   class="
+        sidebar--active__show
+        p-2.5
+        border-gray-500
+        w-full
+        flex
+        items-center
+        justify-center">
+        <span class="material-icons-outlined">sort</span>
+      </router-link>
+    </div>
+
+    <div
+      :class="{'hover:bg-zinc-600 duration-300' : sidebarActive}"
+      class="sidebar--active__item accordion" id="sidenavProjectSettings">
+
+      <router-link to="/" class="sidebar--active__show w-10 h-10 overflow-hidden flex justify-center items-center hover:bg-zinc-600 duration-300">
+        <img src="../assets/img/avatar-default.png" alt="avatar">
+      </router-link>
+
+      <button class="
+        accordion-button
+        collapsed
+        sidebar__user__button
+        sidebar--active__hide
+        w-full
+        flex
+        items-center
+        gap-x-2.5
+        p-2.5
+        border-b
+        border-gray-500
+        hover:bg-zinc-600
+        duration-300"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#sidenavProjectSettingsMenu"
+              aria-expanded="false"
+              aria-controls="sidenavProjectSettingsMenu">
+
+        <span class="material-icons-outlined">
+        settings
+        </span>
+
+        <p>Настройки</p>
+      </button>
+      <div id="sidenavProjectSettingsMenu" class="sidebar--active__hide accordion-collapse collapse max-h-80 overflow-auto border-b border-gray-500"
+           data-bs-parent="#sidenavProjectSettings">
+        <ul class="p-2.5 text-gray-100">
+          <li>
+            <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              Основное
+            </router-link>
+          </li>
+          <li>
+            <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              Синхронизации
+            </router-link>
+          </li>
+          <li>
+            <router-link class="flex items-center gap-1.5 w-full text-sm leading-none hover:bg-zinc-600 p-1.5 duration-300 rounded" to="/">
+              Интеграция
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -80,7 +356,7 @@ export default {
   name: "Sidebar",
   data() {
     return {
-      sidebarMini: false
+      sidebarActive: false
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,49 +5,269 @@
     <section class="w-full">
       <p>bla bla</p>
 
-<!--      <div class="accordion" id="accordionExample">-->
-<!--        <div class="accordion-item bg-white border border-gray-200">-->
-<!--          <h2 class="accordion-header mb-0" id="headingOne">-->
-<!--            <button class=" accordion-button w-full"-->
-<!--                    type="button"-->
-<!--                    data-bs-toggle="collapse"-->
-<!--                    data-bs-target="#collapseOne"-->
-<!--                    aria-expanded="true"-->
-<!--                    aria-controls="collapseOne">-->
-<!--              Accordion Item #1-->
-<!--            </button>-->
-<!--          </h2>-->
-<!--          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"-->
-<!--               data-bs-parent="#accordionExample">-->
-<!--            <div class="accordion-body py-4 px-5">-->
-<!--              <strong>This is the first item's accordion body.</strong> It is shown by default,-->
-<!--              until the collapse plugin adds the appropriate classes that we use to style each-->
-<!--              element. These classes control the overall appearance, as well as the showing and-->
-<!--              hiding via CSS transitions. You can modify any of this with custom CSS or overriding-->
-<!--              our default variables. It's also worth noting that just about any HTML can go within-->
-<!--              the <code>.accordion-body</code>, though the transition does limit overflow.-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
+      <div class="flex justify-center">
+        <div>
+          <div class="dropdown relative">
+            <button
+              class="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown button
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="caret-down"
+                class="w-2 ml-2"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                ></path>
+              </svg>
+            </button>
+            <ul
+              class="
+          dropdown-menu
+          min-w-max
+          absolute
+          hidden
+          bg-white
+          text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+              aria-labelledby="dropdownMenuButton1"
+            >
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Action</a
+                >
+              </li>
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Another action</a
+                >
+              </li>
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Something else here</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 
-      <div class="accordion" id="accordionExample">
-        <button class=" accordion-button w-full"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne">
-          Accordion Item #1
-        </button>
-        <div id="collapseOne" class="accordion-collapse collapse show"
-             data-bs-parent="#accordionExample">
-          <strong>This is the first item's accordion body.</strong> It is shown by default,
-          until the collapse plugin adds the appropriate classes that we use to style each
-          element. These classes control the overall appearance, as well as the showing and
-          hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-          our default variables. It's also worth noting that just about any HTML can go within
-          the <code>.accordion-body</code>, though the transition does limit overflow.
+      <div class="flex justify-center">
+        <div>
+          <div class="dropend relative">
+            <button
+              class="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+              type="button"
+              id="dropdownMenuButton1e"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropend
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="caret-right"
+                class="w-1.5 ml-2"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 192 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
+                ></path>
+              </svg>
+            </button>
+            <ul
+              class="
+          dropdown-menu
+          min-w-max
+          absolute
+          hidden
+          bg-white
+          text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+              aria-labelledby="dropdownMenuButton1e"
+            >
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Action</a
+                >
+              </li>
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Another action</a
+                >
+              </li>
+              <li>
+                <a
+                  class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                  href="#"
+                >Something else here</a
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
